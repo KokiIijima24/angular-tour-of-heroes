c0b9269ffb11c31ee0cedfdb7cef1cc48125ab8c
JBS\koiijima <koki.iijima@jbs.com>
2022-01-07 13:59:00 +0900

    [chapter5] ナビゲーションの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ZippyBasicComponent } from './zippy-basic/zippy-basic.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, ZippyBasicComponent, MessagesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, ZippyBasicComponent, MessagesComponent, DashboardComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,8 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-
   constructor(private heroService: HeroService) {}
   heroes: Hero[] = [];
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
